dc6cdaa59966aa0469de57e0ad8a4a74970614c7
Shawn Wilkinson <me@super3.org>
2013-05-16 01:46:40 -0400

    Flask Index Template Test.

--- CryptoTapV2.py
+++ CryptoTapV2.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from DripRequest import *
+from random import randrange
+from flash import render_template
 app = Flask(__name__)
 
 # Globals
@@ -10,23 +12,31 @@ DEFAULT_SEND_VAL = 0.0001
 # Helper Functions
 def sub_cypher(num, offset):
 	"""Number substitution offset cypher. Don't use offset values 0-9."""
-	# rotate((ip % sum1bits(ip) ), sum0bits(ip))
+	# Implement Better Cypher: rotate((ip % sum1bits(ip) ), sum0bits(ip))
 	return [(abs(int(x) - offset)%10) if x.isdigit() else '.' for x in num]
 
 def get_index(form_submit_status = None):
 	"""Displays the default index page, or a success/error page."""
-	# captcha = (random.randrange(1, 15), random.randrange(1, 15))
-	# captcha_awns = captcha[0] + captcha[1]
-	# recent_drips = Database(DATABASE_FILE, DATABASE_TABLE).get_recent()
-	# recent_drips_html = [get_html(x[1], x[2], x[5]) for x in recent_drips if True]
-	# recent_drips_html = ''.join(map(str, recent_drips_html))
+	captcha = (randrange(1, 15), randrange(1, 15))
+	captcha_awns = captcha[0] + captcha[1]
+	recent_drips = Database(DATABASE_FILE, DATABASE_TABLE).get_recent()
+	recent_drips_html = [get_html(x[1], x[2], x[5]) for x in recent_drips if True]
+	recent = ''.join(map(str, recent_drips_html))
+	# $def with (recent, form_submit, captcha, captcha_awns)
+	return render_template('index.html', recent=recent, form_submit=form_submit,
+						   captcha=captcha, captcha_awns=captcha_awns)
 
-	return render_template('index.html')
+def send_coins():
+	"""Sends queued coins."""
+	data = Database(DATABASE_FILE, DATABASE_TABLE)
+	for i in data.get_unsent():
+		DripRequest(i[1], i[3], i[4], i[2], i[0]).send(DEFAULT_SEND_VAL, data)
+	return "Sent!"
 
 # Routes
 @app.route('/')
 def index():
-	get_index()
+	return get_index()
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0', port=80, debug=True)
