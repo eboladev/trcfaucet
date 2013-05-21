dc55115f6b991175e4e0bf3bc447e6cb80f870a2
super3 <me@super3.org>
2013-05-21 02:05:20 -0400

    Update number

--- CryptoTap.py
+++ CryptoTap.py
@@ -225,7 +225,7 @@ def get_index(form_submit_status = None):
 	recent = ''.join(map(str, recent))
 	
 	cur = g.db.execute('SELECT Count(*) FROM drip_request')
-	stats = 3349 + int(cur.fetchone()[0])
+	stats = 4100 + int(cur.fetchone()[0])
 
 	return render_template('index.html', recent=recent,
 						   form_submit=form_submit_status, captcha=captcha,
