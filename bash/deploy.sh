71f68904e18cc6241261f2f313afe4e2daecff70
super3 <me@super3.org>
2013-06-30 17:21:33 -0400

    Updated Bash Files

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,3 +1,3 @@
 rm -rf ~/CryptoTap
-git clone https://github.com/super3/CryptoTap.git
+git clone https://github.com/super3/trcfaucet.git
 python ~/CryptoTap/CryptoTap.py
--- bash/install.sh
+++ bash/install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install build-essential libboost-all-dev libssl-dev libdb-dev libdb4.8++-dev libglib2.0-dev
+apt-get -y install build-essential libboost-all-dev libdb5.1++-dev libssl-dev
 apt-get update
 apt-get -y install git apt-file python3 screen sqlite3 python-setuptools python-flask
 apt-get update
@@ -14,13 +14,4 @@ mkdir /root/.terracoin/
 echo "rpcuser=CHANGETHIS" >> /root/.terracoin/terracoin.conf
 echo "rpcpassword=CHANGETHIS" >> /root/.terracoin/terracoin.conf
 
-sleep 5
 terracoind -daemon
-terracoind getinfo
-terracoind getbalance
-terracoind getnewaddress
-
-cd ~/
-git clone https://github.com/super3/CryptoTap.git
-cd ~/CryptoTap
-python CryptoTap.py 80
